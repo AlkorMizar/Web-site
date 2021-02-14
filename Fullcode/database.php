0913f07e402e319c1bb9432ceaf0a9e52ad44cc3
AlkorMizar <anisa15012002@gmail.com>
2021-02-15 00:31:30 +0300

    ver 1.0.1.2

--- Fullcode/database.php
+++ Fullcode/database.php
@@ -3,12 +3,12 @@
 
 	function initDatabase($table,&$link){
 		$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
+		print_r($url);
 		$server = $url["host"];
 		$username = $url["user"];
 		$password = $url["pass"];
 		$db = substr($url["path"], 1);
-		
+
 		$link = new mysqli($server, $username, $password, $db);
 		return mysqli_connect_errno($link);
 	};
